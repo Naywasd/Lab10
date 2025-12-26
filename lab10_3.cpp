5c56f2070a1e754e56220103060fe5db3682f812
Naywasd <sermsub2549@gmail.com>
2025-12-26 13:15:51 +0700

    Implement data processing from score.txt

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,32 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+        int count = 0;
+        float sum = 0;
+        float sum_of_square = 0;
+        string textline;
+        ifstream source("score.txt");
+
+        while (source >> textline)
+        {
+                float value = stof(textline);
+                sum += value;
+                sum_of_square += value * value;
+                count++;
+        }
+
+        cout << "Number of data = " << count << "\n";
+        cout << setprecision(3);
+        cout << "Mean = " << sum / count << "\n";
+        cout << "Standard deviation = "
+             << sqrt((sum_of_square / count) - pow(sum / count, 2));
+
+        return 0;
+}
